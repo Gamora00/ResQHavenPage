7a99774083e1ef7d17d7d02e67d76fa3f975657f
Gamora00 <almodieljethro16@gmail.com>
2026-03-16 00:11:28 +0800

    fixed token

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const BASE_URL = 'https://resqhavenbackend-production.up.railway.app'
+const BASE_URL = 'https://resqhavenbackend-production.up.railway.app'
 
-    const BASE_URL = 'http://localhost:5000'
+    //const BASE_URL = 'http://localhost:5000'
 
 
 // GET request
